57de9a3b8920e1f87605a28a52fa9b0e49e4e4b8
himichun <nekto55555@gmail.com>
2018-10-24 21:23:38 +0300

    wow

--- FB2reader.js
+++ FB2reader.js
@@ -10,6 +10,7 @@ class FB2Reader{
 		this.encoding = "UTF-8"
 
 		this.settingsEl = this.generateAndRenderSettingsEl()
+		this.navigationEls = this.generateAndRenderNavigationEls()
 
 		this.bookFile = null
 		this.xml
@@ -18,6 +19,8 @@ class FB2Reader{
 
 		this.fileReader = new FileReader()
 		this.fileReader.onloadend = ()=>this.checkEncoding()
+
+		this.updateNavigation([ "openBook" ])
 	}
 
 	openBook(file){
@@ -53,15 +56,23 @@ class FB2Reader{
 			.map((chapter, number) => {
 				chapter = chapter.replace(/(?:^[\n\r\s]*<section.*?>)|(?:<\/section>[\n\r\s]*$)/g, "")
 				chapter = {
-					title: chapter.match(/<title.*?>([\s\S]*?)<\/title>/)
+					title:
+						chapter.match(/<title.*?>([\s\S]*?)<\/title>/)
 						? chapter.replace(/<title.*?>([\s\S]*?)<\/title>[\s\S]*$/, "$1")
 						: "",
-					content: chapter.replace(/<title.*?>[\s\S]*?<\/title>/, ""),
+					content: chapter
+						.replace(/^[\s\n\r\t]*<title.*?>[\s\S]*?<\/title>/, "")
+						.replace(/<empty-line\s*\/>/g, "<p class=\"empty-line\"></p>"),
 					number: number
 				}
 				return chapter
 			})
 
+		this.bookFile = null
+		this.xml = null
+		// this.fileReader.result = null
+
+		this.settingsEl.className = ""
 		this.openContents()
 	}
 
@@ -77,7 +88,6 @@ class FB2Reader{
 		for(const chapter of this.chapters){
 			const li = document.createElement("li")
 			li.className = "button"
-			console.log(chapter.content.substr(0,100).replace(/<.*?>/g, "").substr(0,60) + "...")
 
 			if(chapter.title.length > 0)
 				li.innerHTML = chapter.title.replace(/<.*?>/g, "")
@@ -85,85 +95,36 @@ class FB2Reader{
 				li.innerHTML = chapter.content.substr(0,100).replace(/(<.*?>)|(<.*[^>]$)/g, "").substr(0,60) + "..."
 				
 			li.addEventListener("click", e=>{
-				if(e.button === 0)
+				if(e.button === 0){
 					this.openChapter(chapter.number)
+					this.animateBook("bottom to top")
+				}
 			})
 
 			list.appendChild(li)
 		}
 
-		const navigation = document.createElement("nav")
-		//
-		const settings = document.createElement("button")
-		settings.className = "settings"
-		settings.innerHTML = "<img src=\"images/gear.svg\" alt=\"\">"
-		settings.addEventListener("click", e=>{
-			if(e.button === 0)
-				this.openSettings()
-		})
-		//
-		navigation.appendChild(settings)
-
-		this.settingsEl.className = ""
-		this.renderPage(title, list, navigation)
+		this.currentChapter = null
+		this.updateNavigation([ "settings", "openBook" ])
+		this.renderPage(title, list)
+		this.animateBook("bottom to top")
+		window.scrollTo(0, 0)
 	}
 
 	openChapter(number){
 		console.log("purr " + number)
 
 		const chapter = this.chapters[number]
-
+		
 		const title = document.createElement("h2")
 		title.innerHTML = chapter.title
-
+		
 		const content = document.createElement("section")
 		content.innerHTML = chapter.content
-
-		const navigation = document.createElement("nav")
-		//
-		const prev = document.createElement("button")
-		prev.className = "prev"
-		prev.innerHTML = "<img src=\"images/arrow.svg\" alt=\"\">"
-		prev.addEventListener("click", e=>{
-			if(e.button === 0)
-				if(number == 0)
-					this.openContents()
-				else
-					this.openChapter(number - 1)
-		})
-		//
-		const contents = document.createElement("button")
-		contents.className = "contents"
-		contents.innerHTML = "<img src=\"images/list.svg\" alt=\"\">"
-		contents.addEventListener("click", e=>{
-			if(e.button === 0)
-				this.openContents()
-		})
-		//
-		const settings = document.createElement("button")
-		settings.className = "settings"
-		settings.innerHTML = "<img src=\"images/gear.svg\" alt=\"\">"
-		settings.addEventListener("click", e=>{
-			if(e.button === 0)
-				this.openSettings()
-		})
-		//
-		const next = document.createElement("button")
-		next.className = "next"
-		next.innerHTML = "<img src=\"images/arrow.svg\" alt=\"\">"
-		next.addEventListener("click", e=>{
-			if(e.button === 0)
-				if(number == this.chapters.length - 1)
-					this.openContents()
-				else
-					this.openChapter(number + 1)
-		})
-		//
-		navigation.appendChildren(prev, contents, settings, next)
-
-		this.settingsEl.className = ""
-
-		this.renderPage(title, content, navigation)
+		
+		this.currentChapter = number
+		this.updateNavigation([ "prev", "contents", "settings", "next" ])
+		this.renderPage(title, content)
 		window.scrollTo(0, 0)
 	}
 
@@ -174,6 +135,63 @@ class FB2Reader{
 			this.settingsEl.classList.add("open")
 	}
 
+
+	animateBook(how){
+		let axis, start, end
+
+		switch (how) {
+			case "right to left":
+				axis = "left",
+				start = "32px",
+				end = 0
+				break;
+			case "left to right":
+				axis = "left",
+				start = "-32px",
+				end = 0
+				break;
+			case "bottom to top":
+				axis = "top",
+				start = "32px",
+				end = 0
+				break;
+			default:
+				break;
+		}
+		
+		const animation = {
+			easing: [ "cubic-bezier(0.0, 0.0, 0.2, 1)", "cubic-bezier(0.0, 0.0, 0.2, 1)" ]
+		}
+		animation[axis] = [start, end]
+
+		this.bookEl.animate(animation, 200)
+	}
+
+
+	updateNavigation(visibleElements){ //[ "___", "___" ]
+		let first = null
+		let last  = null
+
+		for(const name of Object.keys(this.navigationEls)){
+			const element = this.navigationEls[name]
+	
+			element.classList.remove("first")
+			element.classList.remove("last")
+
+			if(visibleElements.includes(name)){
+				if(!first) first = element
+				last = element
+				element.classList.remove("hidden")
+			}
+			else
+				element.classList.add("hidden")
+		}
+		if(first)
+			first.classList.add("first")
+		if(last)
+			last.classList.add("last")
+	}
+
 	generateAndRenderBookEl(){
 		const book = document.createElement("div")
 		book.id = "book"
@@ -181,47 +199,114 @@ class FB2Reader{
 		return book
 	}
 
+	generateAndRenderNavigationEls(){
+		const navigation = document.createElement("nav")
+		//
+		const prev = document.createElement("button")
+		prev.className = "prev"
+		prev.innerHTML = "<img src=\"images/arrow.svg\" alt=\"\">"
+		prev.onclick = e => {
+			if(e.button === 0)
+				if(this.currentChapter == null) return
+				else if(this.currentChapter === 0)
+					this.openContents()
+				else{
+					this.openChapter(this.currentChapter - 1)
+					this.animateBook("left to right")
+				}
+		}
+		//
+		const contents = document.createElement("button")
+		contents.className = "contents"
+		contents.innerHTML = "<img src=\"images/list.svg\" alt=\"\">"
+		contents.onclick = e => {
+			if(e.button === 0)
+				this.openContents()
+		}
+		//
+		const openBook = document.createElement("button")
+		openBook.className = "openBook"
+		openBook.innerHTML = "<img src=\"images/book.svg\" alt=\"\">"
+		openBook.onclick = e => {
+			if(e.button === 0){
+				const file = document.createElement("input")
+				file.type = "file"
+				file.setAttribute("accept",".fb2")
+				file.onchange = () => {
+					this.openBook(file.files[0])
+				}
+				file.click()
+			}
+		}
+		//
+		const settings = document.createElement("button")
+		settings.className = "settings"
+		settings.innerHTML = "<img src=\"images/gear.svg\" alt=\"\">"
+		settings.onclick = e => {
+			if(e.button === 0)
+				this.openSettings()
+		}
+		//
+		const next = document.createElement("button")
+		next.className = "next"
+		next.innerHTML = "<img src=\"images/arrow.svg\" alt=\"\">"
+		next.onclick = e => {
+			if(e.button === 0)
+				if(this.currentChapter == null) return
+				else if(this.currentChapter === this.chapters.length - 1)
+					this.openContents()
+				else{
+					this.openChapter(this.currentChapter + 1)
+					this.animateBook("right to left")
+				}
+		}
+		//
+		navigation.appendChildren(prev, contents, openBook, settings, next)
+		document.body.appendChild(navigation)
+		return { prev, contents, openBook, settings, next }
+	}
+
 	generateAndRenderSettingsEl(){
 		const settings = document.createElement("div")
 		settings.id = "settings"
-		settings.className = "open";
+		// settings.className = "open"
 
-		(()=>{
-			const fileBlock = document.createElement("div")
-			fileBlock.className = "button-block"
-			//
-			const label = document.createElement("label")
-			label.className = "button"
-			//
-			const labelText = document.createElement("span")
-			labelText.innerText = "choose an fb2 book"
-			label.appendChild(labelText)
-			//
-			const file = document.createElement("input")
-			file.type = "file"
-			file.name = "input-file"
-			file.setAttribute("accept",".fb2")
-			label.appendChild(file)
-			//
-			const button = document.createElement("button")
-			button.innerText = "open"
-			button.disabled = true
-			//
-			file.addEventListener("change", ()=>{
-				button.disabled = false
-				labelText.innerText = file.files[0].name
-			})
-			button.addEventListener("click", e=>{
-				if(e.button != 0) return
-				button.disabled = true
-				this.openBook(file.files[0])
-			})
-			//
-			fileBlock.appendChildren(label, button)
-			settings.appendChild(fileBlock)
-		})();
+		// ;(()=>{
+		// 	const fileBlock = document.createElement("div")
+		// 	fileBlock.className = "button-block"
+		// 	//
+		// 	const label = document.createElement("label")
+		// 	label.className = "button"
+		// 	//
+		// 	const labelText = document.createElement("span")
+		// 	labelText.innerText = "choose an fb2 book"
+		// 	label.appendChild(labelText)
+		// 	//
+		// 	const file = document.createElement("input")
+		// 	file.type = "file"
+		// 	file.name = "input-file"
+		// 	file.setAttribute("accept",".fb2")
+		// 	label.appendChild(file)
+		// 	//
+		// 	const button = document.createElement("button")
+		// 	button.innerText = "open"
+		// 	button.disabled = true
+		// 	//
+		// 	file.onchange = () => {
+		// 		button.disabled = false
+		// 		labelText.innerText = file.files[0].name
+		// 	}
+		// 	button.onclick = e => {
+		// 		if(e.button != 0) return
+		// 		button.disabled = true
+		// 		this.openBook(file.files[0])
+		// 	}
+		// 	//
+		// 	fileBlock.appendChildren(label, button)
+		// 	settings.appendChild(fileBlock)
+		// })()
 
-		(()=>{
+		;(()=>{
 			const align = document.createElement("div")
 			align.className = "button-block"
 			//
@@ -237,10 +322,14 @@ class FB2Reader{
 			const right = document.createElement("button")
 			right.innerHTML = "<img src=\"images/align-right.svg\" alt=\"\">"
 			//
-			align.addEventListener("click", e=>{
+			align.onclick = e => {
 				if(e.button != 0) return
+
+				let target = e.target.closest("button")
+				if(!target) return
+
 				let align = null
-				switch (e.target) {
+				switch (target) {
 					case left   : align = "left" ;break;
 					case center : align = "center" ;break;
 					case justify: align = "justify" ;break;
@@ -248,9 +337,50 @@ class FB2Reader{
 					default: return
 				}
 				this.bookEl.style.textAlign = align
-			})
+			}
 			align.appendChildren(left, center, justify, right)
 			settings.appendChild(align)
+		})()
+
+		;(()=>{
+			const font = document.createElement("div")
+			font.className = "button-block"
+			//
+			const serif = document.createElement("button")
+			serif.className = "serif"
+			serif.innerText = "Aa"
+			//
+			const sans = document.createElement("button")
+			sans.className = "sans"
+			sans.innerText = "Aa"
+			//
+			const size = document.createElement("input")
+			size.type = "number"
+			size.className = "button"
+			size.maxLength = 2
+			size.placeholder = 16
+			size.value = 16
+			size.onchange = () => {
+				this.bookEl.style.fontSize = size.value + "px"
+			}
+			//
+			font.onclick = e => {
+				if(e.button != 0) return
+
+				const target = e.target.closest("button")
+				if(!target) return
+
+				let fontName
+				switch (target) {
+					case serif: fontName = "serif"		;break;
+					case sans : fontName = "sans-serif"	;break;
+					default: return
+				}
+				this.bookEl.style.fontFamily = fontName
+			}
+			//
+			font.appendChildren(serif, sans, size)
+			settings.appendChild(font)
 		})()
 
 		document.body.appendChild(settings)
